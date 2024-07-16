ca94955c78a6bfb0a96e7515cf22da7577a9d52e
alexandrerosapc <alexandrerosasoares3@gmail.com>
2024-07-16 09:43:08 -0300

    Parte de adicionar usuário com os status code

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const server = express()
+
+server.use(express.json())
+
+const PORT = 5000
+
+const usuarios = []
+
+const tweets = []
+
+server.post("/sing-up", (req, res) => {
+    const dadosUsuario = req.body
+
+    const buscarUsuarioExistente = usuarios.find(item => item.username === dadosUsuario.username)
+
+    if (buscarUsuarioExistente) return res.status(409).send("Usuário já existente")
+
+    usuarios.push(dadosUsuario)
+
+    console.log(usuarios)
+
+    res.status(201).send("Usuário criado com sucesso")
+})
+
+server.listen(PORT, () => { console.log(`Servidor está funcionando na porta ${PORT}`) })
